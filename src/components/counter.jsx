39794f0ee3991ae7d9f53cc0d518c42047c90652
svs57 <svs57@cornell.edu>
2021-01-08 22:07:42 -0600

    lists impl

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,44 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    value: this.props.value,
+  };
+
+  // constructor(){
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
   render() {
+    console.log(this.props);
     return (
       <div>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        {this.props.children}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className='btn btn-secondary btn-sm'
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value == 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value == 0 ? "Zero" : value;
   }
 }
 
